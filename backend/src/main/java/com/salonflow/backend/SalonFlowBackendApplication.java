964871866e269705fa92599810a09022485c3941
Guilherme <guilhermeggr23@gmail.com>
2026-01-26 22:14:53 -0300

    chore: initial springboot setup (#1)

---
+++ backend/src/main/java/com/salonflow/backend/SalonFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.salonflow.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonFlowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonFlowBackendApplication.class, args);
+	}
+
+}
