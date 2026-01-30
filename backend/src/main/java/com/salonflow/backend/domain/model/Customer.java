9628925e2c10d93760c3f13dc6d44c4f978502fb
Guilherme <guilhermeggr23@gmail.com>
2026-01-30 16:50:31 -0300

    feat: added attributes, getters, setters, no args constructor and all args constructor on Customer model (#6)

--- backend/src/main/java/com/salonflow/backend/domain/model/Customer.java
+++ backend/src/main/java/com/salonflow/backend/domain/model/Customer.java
@@ -1,4 +1,34 @@
 package com.salonflow.backend.domain.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    private String nome;
+
+    @NotNull
+    private String telefone;
+
+    private LocalDate created_at;
+
+
 }
