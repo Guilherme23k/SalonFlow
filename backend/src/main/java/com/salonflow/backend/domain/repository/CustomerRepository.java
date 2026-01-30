99886950597cbbe5f33dda0eacb1913030268ec7
Guilherme <guilhermeggr23@gmail.com>
2026-01-30 17:22:24 -0300

    feat: added custom query findByTelefone (#6)

--- backend/src/main/java/com/salonflow/backend/domain/repository/CustomerRepository.java
+++ backend/src/main/java/com/salonflow/backend/domain/repository/CustomerRepository.java
@@ -1,4 +1,17 @@
 package com.salonflow.backend.domain.repository;
 
-public interface CustomerRepository {
+import com.salonflow.backend.domain.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CustomerRepository extends JpaRepository<CustomerRepository, UUID> {
+
+
+    @Query(value = "SELECT c FROM Customer c WHERE c.telefone = :telefone")
+    List<Customer> findByTelefone(@Param("telefone") String telefone);
+
 }
