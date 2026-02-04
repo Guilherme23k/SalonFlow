bfb9f9cbda4494f08c30baa7728db4776ed279ec
Guilherme <guilhermeggr23@gmail.com>
2026-02-03 23:34:26 -0300

    feat: added findByNameContainingIgnoreCase (#6)

--- backend/src/main/java/com/salonflow/backend/domain/repository/CustomerRepository.java
+++ backend/src/main/java/com/salonflow/backend/domain/repository/CustomerRepository.java
@@ -14,4 +14,7 @@ public interface CustomerRepository extends JpaRepository<CustomerRepository, UU
     @Query(value = "SELECT c FROM Customer c WHERE c.phone = :phone")
     List<Customer> findByPhone(@Param("telefone") String phone);
 
+    @Query(value = "SELECT c FROM Customer c WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+    List<Customer> findByNameContainingIgnoreCase(String name);
+
 }
