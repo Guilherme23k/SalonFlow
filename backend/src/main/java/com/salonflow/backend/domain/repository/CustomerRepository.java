3aab9baff0c326e4af3e9457e5fed2988938c422
Guilherme <guilhermeggr23@gmail.com>
2026-02-06 18:39:33 -0300

    refactor: change @Param to correct sql field (#6)

--- backend/src/main/java/com/salonflow/backend/domain/repository/CustomerRepository.java
+++ backend/src/main/java/com/salonflow/backend/domain/repository/CustomerRepository.java
@@ -6,13 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
 
     @Query(value = "SELECT c FROM Customer c WHERE c.phone = :phone")
-    List<Customer> findByPhone(@Param("telefone") String phone);
+    Optional<Customer> findByPhone(@Param("phone") String phone);
 
     @Query(value = "SELECT c FROM Customer c WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%'))")
     List<Customer> findByNameContainingIgnoreCase(String name);
