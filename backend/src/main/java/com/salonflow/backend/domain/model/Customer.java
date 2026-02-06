eae78334d2e407bc406c1a72818177755b6fa1c8
Guilherme <guilhermeggr23@gmail.com>
2026-02-05 21:40:47 -0300

    feat: adds @BRPhoneNumber on model Customer (#6)

--- backend/src/main/java/com/salonflow/backend/domain/model/Customer.java
+++ backend/src/main/java/com/salonflow/backend/domain/model/Customer.java
@@ -1,5 +1,6 @@
 package com.salonflow.backend.domain.model;
 
+import com.salonflow.backend.validations.interfaces.BRPhoneNumber;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -27,6 +28,7 @@ public class Customer {
     private String name;
 
     @NotNull
+    @BRPhoneNumber
     private String phone;
 
     private LocalDateTime created_at;
