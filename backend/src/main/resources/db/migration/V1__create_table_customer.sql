b32b1f25240a9d160898c64814fa0c6321e56882
Guilherme <guilhermeggr23@gmail.com>
2026-02-06 18:42:17 -0300

    fix: fixes sql syntax (#6)

--- backend/src/main/resources/db/migration/V1__create_table_customer.sql
+++ backend/src/main/resources/db/migration/V1__create_table_customer.sql
@@ -1,6 +1,6 @@
 CREATE TABLE customers (
-    id UUID NOT NULL PRIMARY KEY
-    nome VARCHAR(255)
-    telefone UNIQUE
-    criado_em TIMESTAMP
+    id UUID NOT NULL PRIMARY KEY,
+    nome VARCHAR(255),
+    telephone VARCHAR(11) UNIQUE,
+    create_em TIMESTAMP
 )
