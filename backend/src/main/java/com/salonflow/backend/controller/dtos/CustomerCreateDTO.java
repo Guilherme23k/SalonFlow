7e4a19d51fccfd58d2b359e84dfa8f73c6712096
Guilherme <guilhermeggr23@gmail.com>
2026-02-06 14:30:48 -0300

    refactor: change @Patter with regex to custom BRPhoneNumber custom bean (#6)

--- backend/src/main/java/com/salonflow/backend/controller/dtos/CustomerCreateDTO.java
+++ backend/src/main/java/com/salonflow/backend/controller/dtos/CustomerCreateDTO.java
@@ -1,7 +1,9 @@
 package com.salonflow.backend.controller.dtos;
 
+import com.salonflow.backend.validations.interfaces.BRPhoneNumber;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record CustomerCreateDTO(@NotBlank String name,
-                                @NotBlank @Pattern(regexp = "^\\s*(\\d{2}|\\d{0})[-. ]?(\\d{5}|\\d{4})[-. ]?(\\d{4})[-. ]?\\s*$\n") String phone) { }
+                                @NotBlank @BRPhoneNumber String phone) { }
+
