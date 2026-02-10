2866c2e810fb524df735352dff3eb85ac8c7d31c
Guilherme <guilhermeggr23@gmail.com>
2026-02-10 20:57:25 -0300

    refactor: change type of totalAgendamentos to Optional because this attribute can be nullable or 0 (#6)

--- backend/src/main/java/com/salonflow/backend/controller/dtos/CustomerListDTO.java
+++ backend/src/main/java/com/salonflow/backend/controller/dtos/CustomerListDTO.java
@@ -1,10 +1,10 @@
 package com.salonflow.backend.controller.dtos;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public record CustomerListDTO(UUID id,
                               String name,
                               String phone,
-                              List<Integer> totalAgendamentos) {
+                              Optional<Integer> totalAgendamentos) {
 }
