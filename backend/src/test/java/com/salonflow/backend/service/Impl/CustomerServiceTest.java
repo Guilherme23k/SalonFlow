2929206dd34c207aeaa66c014a937f74d92dc005
Guilherme <guilhermeggr23@gmail.com>
2026-02-10 20:58:28 -0300

    feat: create test should transfer model to DTO and list those objects (#6)

--- backend/src/test/java/com/salonflow/backend/service/Impl/CustomerServiceTest.java
+++ backend/src/test/java/com/salonflow/backend/service/Impl/CustomerServiceTest.java
@@ -1,8 +1,11 @@
 package com.salonflow.backend.service.Impl;
 
 import com.salonflow.backend.controller.dtos.CustomerCreateDTO;
+import com.salonflow.backend.controller.dtos.CustomerListDTO;
 import com.salonflow.backend.domain.model.Customer;
 import com.salonflow.backend.domain.repository.CustomerRepository;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,8 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -70,6 +72,35 @@ class CustomerServiceTest {
         assertNotNull(results.getId());
         assertEquals(dto.phone(), results.getPhone());
         verify(customerRepository, never()).save(any());
+
+    }
+
+    @Test
+    public void shouldTransferToDTOAndListCustomerListDTO(){
+
+        Customer id1Customer = new Customer();
+        id1Customer.setId(UUID.randomUUID());
+        id1Customer.setName("Guigas");
+        id1Customer.setPhone("11902939485");
+
+        Customer id2Customer = new Customer();
+        id2Customer.setId(UUID.randomUUID());
+        id2Customer.setName("Guigas");
+        id2Customer.setPhone("11902939485");
+
+
+        when(customerRepository.findAll()).thenReturn(List.of(id1Customer, id2Customer));
+
+        CustomerListDTO customerListDTO1 = new CustomerListDTO(id1Customer.getId(), id1Customer.getName(), id1Customer.getPhone(), Optional.empty());
+        CustomerListDTO customerListDTO2 = new CustomerListDTO(id2Customer.getId(), id2Customer.getName(), id2Customer.getPhone(), Optional.empty());
+
+        List<CustomerListDTO> expectedList = List.of(customerListDTO1, customerListDTO2);
+
+        List<CustomerListDTO> realList = customerService.listAllCustomers();
+
+        assertThat(realList)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedList);
 
     }
 
