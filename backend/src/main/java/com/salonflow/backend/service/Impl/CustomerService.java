61ccab8dafeab88a2fd88e4a5398a0e6186ffed2
Guilherme <guilhermeggr23@gmail.com>
2026-02-10 22:32:56 -0300

    feat: create findCustomerByPhone (#6)

--- backend/src/main/java/com/salonflow/backend/service/Impl/CustomerService.java
+++ backend/src/main/java/com/salonflow/backend/service/Impl/CustomerService.java
@@ -2,13 +2,13 @@ package com.salonflow.backend.service.Impl;
 
 import com.salonflow.backend.controller.dtos.CustomerCreateDTO;
 import com.salonflow.backend.controller.dtos.CustomerListDTO;
+import com.salonflow.backend.controller.dtos.response.CustomerResponseDTO;
 import com.salonflow.backend.domain.model.Customer;
 import com.salonflow.backend.domain.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -49,4 +49,16 @@ public class CustomerService {
     }
 
 
+    public CustomerResponseDTO findCustomerByPhone(String phone) {
+
+
+        return customerRepository.findByPhone(phone)
+                .map(customer -> new CustomerResponseDTO(
+                        customer.getId(),
+                        customer.getName(),
+                        customer.getPhone(),
+                        customer.getCreated_at()
+                )).orElseThrow(() -> new RuntimeException("Customer not found"));
+
+    }
 }
