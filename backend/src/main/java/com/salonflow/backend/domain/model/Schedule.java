283512d481e5ae8604be9e436c744b0684cef02f
Guilherme <guilhermeggr23@gmail.com>
2026-02-11 17:47:53 -0300

    feat: write lombok annotations and attributes (#6)

--- backend/src/main/java/com/salonflow/backend/domain/model/Schedule.java
+++ backend/src/main/java/com/salonflow/backend/domain/model/Schedule.java
@@ -1,4 +1,27 @@
 package com.salonflow.backend.domain.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "schedules")
 public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private LocalDateTime scheduleTime;
+
+    private Boolean done;
+
+
 }
