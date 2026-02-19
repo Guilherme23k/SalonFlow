a7bdb87701e288e7d74e03fbf1f7ffa7e268fd13
Guilherme <guilhermeggr23@gmail.com>
2026-02-19 15:57:13 -0300

    feat: implements CustomerService contracts (#6)

--- backend/src/main/java/com/salonflow/backend/service/Impl/CustomerService.java
+++ backend/src/main/java/com/salonflow/backend/service/Impl/CustomerService.java
@@ -1,4 +1,18 @@
 package com.salonflow.backend.service.Impl;
 
+import com.salonflow.backend.controller.dtos.CustomerCreateDTO;
+import com.salonflow.backend.controller.dtos.CustomerListDTO;
+import com.salonflow.backend.controller.dtos.response.CustomerResponseDTO;
+import com.salonflow.backend.domain.model.Customer;
+
+import java.util.List;
+
 public interface CustomerService {
+
+
+    Customer findOrCreateByTelefone(CustomerCreateDTO dto);
+
+    List<CustomerListDTO> listAllCustomers();
+
+    CustomerResponseDTO findCustomerByPhone(String phone);
 }
