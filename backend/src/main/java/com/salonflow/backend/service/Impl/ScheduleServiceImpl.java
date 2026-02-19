5cb1a446bd7835e39895e60d7c016e7b36bb7d88
Guilherme <guilhermeggr23@gmail.com>
2026-02-19 16:29:12 -0300

    feat: create ScheduleService and ScheduleServiceImpl (#6)

---
+++ backend/src/main/java/com/salonflow/backend/service/Impl/ScheduleServiceImpl.java
@@ -0,0 +1,11 @@
+package com.salonflow.backend.service.Impl;
+
+import com.salonflow.backend.service.ScheduleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduleServiceImpl implements ScheduleService {
+
+
+
+}
---
+++ backend/src/main/java/com/salonflow/backend/service/ScheduleService.java
@@ -0,0 +1,6 @@
+package com.salonflow.backend.service;
+
+import com.salonflow.backend.service.Impl.ScheduleServiceImpl;
+
+public interface ScheduleService {
+}
