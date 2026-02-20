17f2dc8662f7dc5910206d2efaf2b4d006459906
Guilherme <guilhermeggr23@gmail.com>
2026-02-20 15:49:51 -0300

    feat: implements logic for createSchedule  (#6)

--- backend/src/main/java/com/salonflow/backend/service/Impl/ScheduleServiceImpl.java
+++ backend/src/main/java/com/salonflow/backend/service/Impl/ScheduleServiceImpl.java
@@ -1,7 +1,10 @@
 package com.salonflow.backend.service.Impl;
 
 import com.salonflow.backend.controller.dtos.ScheduleCreateDTO;
+import com.salonflow.backend.domain.model.Professional;
 import com.salonflow.backend.domain.model.Schedule;
+import com.salonflow.backend.domain.model.enums.Status;
+import com.salonflow.backend.domain.repository.ProfessionalRepository;
 import com.salonflow.backend.domain.repository.ScheduleRepository;
 import com.salonflow.backend.service.ScheduleService;
 import org.springframework.stereotype.Service;
@@ -10,14 +13,22 @@ import org.springframework.stereotype.Service;
 public class ScheduleServiceImpl implements ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
+    private final ProfessionalRepository professionalRepository;
 
-    public ScheduleServiceImpl(ScheduleRepository scheduleRepository) {
+    public ScheduleServiceImpl(ScheduleRepository scheduleRepository, ProfessionalRepository professionalRepository) {
         this.scheduleRepository = scheduleRepository;
+
+        this.professionalRepository = professionalRepository;
     }
 
-    public Schedule createSchedule(ScheduleCreateDTO scheduleCreateDTO) {
+    public Schedule createSchedule(ScheduleCreateDTO dto) {
 
         Schedule schedule = new Schedule();
+        schedule.setScheduleTime(dto.scheduleTime());
+        Professional prof = professionalRepository.findByName(dto.professionalName())
+                .orElseThrow(() -> new RuntimeException("Professional not found"));
+        schedule.setProfessional(prof);
+        schedule.setStatus(Status.SCHEDULED);
 
         return scheduleRepository.save(schedule);
 
