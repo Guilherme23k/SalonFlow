8e956e259703e5b60da0af0312889a3e148132a9
Guilherme <guilhermeggr23@gmail.com>
2026-02-20 15:53:50 -0300

    refactor: add customerId on ScheduleCreateDTO (#6)

--- backend/src/main/java/com/salonflow/backend/controller/dtos/ScheduleCreateDTO.java
+++ backend/src/main/java/com/salonflow/backend/controller/dtos/ScheduleCreateDTO.java
@@ -1,9 +1,11 @@
 package com.salonflow.backend.controller.dtos;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public record ScheduleCreateDTO(CustomerCreateDTO createDTO,
                                 LocalDateTime scheduleTime,
+                                UUID customerId,
                                 String professionalName,
                                 String professionalService) {
 }
