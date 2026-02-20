779e9eba62b9f1d04c4654e2a8bd1c019a7243dd
Guilherme <guilhermeggr23@gmail.com>
2026-02-20 14:27:25 -0300

    feat: created method createSchedule  (#6)

--- backend/src/main/java/com/salonflow/backend/service/Impl/ScheduleServiceImpl.java
+++ backend/src/main/java/com/salonflow/backend/service/Impl/ScheduleServiceImpl.java
@@ -1,11 +1,25 @@
 package com.salonflow.backend.service.Impl;
 
+import com.salonflow.backend.controller.dtos.ScheduleCreateDTO;
+import com.salonflow.backend.domain.model.Schedule;
+import com.salonflow.backend.domain.repository.ScheduleRepository;
 import com.salonflow.backend.service.ScheduleService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ScheduleServiceImpl implements ScheduleService {
 
+    private final ScheduleRepository scheduleRepository;
 
+    public ScheduleServiceImpl(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
 
+    public Schedule createSchedule(ScheduleCreateDTO scheduleCreateDTO) {
+
+        Schedule schedule = new Schedule();
+
+        return scheduleRepository.save(schedule);
+
+    }
 }
