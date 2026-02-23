7b556405c385c9ede04b9964b6833d4647c7f141
Guilherme <guilhermeggr23@gmail.com>
2026-02-23 16:39:43 -0300

    refactor: add createdAt on CustomerListDTO (#6)

--- backend/src/main/java/com/salonflow/backend/controller/dtos/CustomerListDTO.java
+++ backend/src/main/java/com/salonflow/backend/controller/dtos/CustomerListDTO.java
@@ -1,10 +1,12 @@
 package com.salonflow.backend.controller.dtos;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 public record CustomerListDTO(UUID id,
                               String name,
                               String phone,
+                              LocalDateTime createdAt,
                               Optional<Integer> totalAgendamentos) {
 }
