b79b8825b09a51eaab51539efcb6a701d437872b
Guilherme <guilhermeggr23@gmail.com>
2026-02-23 17:50:45 -0300

    refactor: add static method that convert entity to dto (#6)

---
+++ backend/src/main/java/com/salonflow/backend/controller/dtos/ScheduleDTO.java
@@ -0,0 +1,24 @@
+package com.salonflow.backend.controller.dtos;
+
+import com.salonflow.backend.domain.model.Schedule;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ScheduleDTO(
+                                UUID customerId,
+                                LocalDateTime scheduleTime,
+                                String professionalName,
+                                String serviceName,
+                                Double price) {
+
+    public static ScheduleDTO fromEntity(Schedule schedule){
+        return new ScheduleDTO(
+                schedule.getId(),
+                schedule.getScheduleTime(),
+                schedule.getProfessional().getName(),
+                schedule.getProfessionalServices().getName(),
+                schedule.getProfessionalServices().getPrice()
+        );
+    }
+}
