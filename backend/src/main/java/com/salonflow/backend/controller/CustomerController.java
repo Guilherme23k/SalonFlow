09c915fb413643a1e7679f8eaeeaffe413466751
Guilherme <guilhermeggr23@gmail.com>
2026-03-03 18:15:28 -0300

    feat: create GET customer by phone number (#6)

--- backend/src/main/java/com/salonflow/backend/controller/CustomerController.java
+++ backend/src/main/java/com/salonflow/backend/controller/CustomerController.java
@@ -1,4 +1,28 @@
 package com.salonflow.backend.controller;
 
+import com.salonflow.backend.controller.dtos.CustomerDTO;
+import com.salonflow.backend.controller.dtos.response.CustomerResponseDTO;
+import com.salonflow.backend.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/{phone}")
+    public ResponseEntity<CustomerResponseDTO> findByPhoneNumber(@PathVariable String phone){
+        CustomerResponseDTO customer = customerService.findCustomerByPhone(phone);
+
+        return ResponseEntity.ok(customer);
+    }
+
+
 }
