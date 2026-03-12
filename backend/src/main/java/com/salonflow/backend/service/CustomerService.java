5ee5cdbc36a8005a91729bb39e879ee933ad808f
Guilherme <guilhermeggr23@gmail.com>
2026-03-12 13:08:51 -0300

    refactor: change type of object from CustomerDTO to Customer (#6)

--- backend/src/main/java/com/salonflow/backend/service/CustomerService.java
+++ backend/src/main/java/com/salonflow/backend/service/CustomerService.java
@@ -3,13 +3,14 @@ package com.salonflow.backend.service;
 import com.salonflow.backend.controller.dtos.CustomerCreateDTO;
 import com.salonflow.backend.controller.dtos.CustomerDTO;
 import com.salonflow.backend.controller.dtos.response.CustomerResponseDTO;
+import com.salonflow.backend.domain.model.Customer;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface CustomerService {
 
-    CustomerDTO findOrCreateByTelefone(CustomerCreateDTO dto);
+    Customer findOrCreateByPhone(CustomerCreateDTO dto);
 
     List<CustomerDTO> listAllCustomers();
 
