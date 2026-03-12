65067ed066af7393c01ab593ceda6d4a2c871060
Guilherme <guilhermeggr23@gmail.com>
2026-03-12 17:58:01 -0300

    feat: add find by phone method in CustomerService

--- frontend/frontend/src/app/services/customer-service.ts
+++ frontend/frontend/src/app/services/customer-service.ts
@@ -1,8 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomerService {
+
+  private apiUrl = 'http://localhost:8080/customers/';
+
+  constructor(private http: HttpClient){}
+
+
+  findByPhone(phone : string): Observable<Customer> {
+    return this.http.get<Customer>(this.apiUrl + `${phone}`);
+  }
+
+
   
 }
