e7d060754556c9dcfb9e823bd68aef0a6600f739
Guilherme <guilhermeggr23@gmail.com>
2026-03-13 13:44:58 -0300

    refactor: add more specific rules on flyway migration v8 (#7)

--- backend/src/main/resources/db/migration/V8__cleanup_schedule_redudancy.sql
+++ backend/src/main/resources/db/migration/V8__cleanup_schedule_redudancy.sql
@@ -1,2 +1,7 @@
 ALTER TABLE schedules DROP CONSTRAINT IF EXISTS fk_schedule_service;
 ALTER TABLE schedules DROP COLUMN IF EXISTS professional_services_id;
+
+ALTER TABLE schedules DROP CONSTRAINT IF EXISTS fk_schedule_service;
+ALTER TABLE schedules DROP COLUMN IF EXISTS professional_services_id;
+
+ALTER TABLE schedules RENAME COLUMN customers_id TO customer_id;
