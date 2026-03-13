6d13b3d33d6a5a4692b9a14e80b088b817e598c0
Guilherme <guilhermeggr23@gmail.com>
2026-03-13 19:09:29 -0300

    feat: add create on ProfessionaServiceImpl (#7)

---
+++ backend/src/main/java/com/salonflow/backend/service/Impl/ProfessionalServiceImpl.java
@@ -0,0 +1,17 @@
+package com.salonflow.backend.service.Impl;
+
+import com.salonflow.backend.controller.dtos.professional.ProfessionalCreateDTO;
+import com.salonflow.backend.domain.model.Professional;
+import com.salonflow.backend.domain.repository.ProfessionalRepository;
+import com.salonflow.backend.service.ProfessionalService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfessionalServiceImpl implements ProfessionalService {
+
+    private final ProfessionalRepository professionalRepository;
+
+
+    public Professional create(ProfessionalCreateDTO dto) {
+    }
+}
