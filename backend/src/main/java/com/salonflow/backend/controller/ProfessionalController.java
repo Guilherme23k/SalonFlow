caf1e24ce1e9d98bc4154c86cbdf367a03d7bab1
Guilherme <guilhermeggr23@gmail.com>
2026-03-16 20:06:16 -0300

    feat: add ProfessionalController (#7)

---
+++ backend/src/main/java/com/salonflow/backend/controller/ProfessionalController.java
@@ -0,0 +1,11 @@
+package com.salonflow.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/professionals")
+public class ProfessionalController {
+
+}
