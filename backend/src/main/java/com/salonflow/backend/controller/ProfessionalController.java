e19ec35bd659c493507bf119e1b74f286871278f
Guilherme <guilhermeggr23@gmail.com>
2026-03-17 19:16:40 -0300

    refactor: change create customer (#7)

--- backend/src/main/java/com/salonflow/backend/controller/ProfessionalController.java
+++ backend/src/main/java/com/salonflow/backend/controller/ProfessionalController.java
@@ -1,11 +1,33 @@
 package com.salonflow.backend.controller;
 
+import com.salonflow.backend.controller.dtos.professional.ProfessionalCreateDTO;
+import com.salonflow.backend.controller.dtos.response.ProfessionalResponseDTO;
+import com.salonflow.backend.service.ProfessionalService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/professionals")
 public class ProfessionalController {
+
+
+    @Autowired
+    private ProfessionalService professionalService;
+
+    @PostMapping
+    public ResponseEntity<ProfessionalResponseDTO> createProfessional(@RequestBody @Valid ProfessionalCreateDTO dto){
+
+        ProfessionalResponseDTO responseDTO = ProfessionalResponseDTO.fromCreateToResponse(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+
+    }
 
 }
