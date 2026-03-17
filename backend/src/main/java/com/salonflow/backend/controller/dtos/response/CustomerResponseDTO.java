6090099e4b6a7d88aa6acbc7d7d4d19f3c55c5ca
Guilherme <guilhermeggr23@gmail.com>
2026-03-17 19:12:07 -0300

    refactor: changed arguments on customer response DTO (#7)

--- backend/src/main/java/com/salonflow/backend/controller/dtos/response/CustomerResponseDTO.java
+++ backend/src/main/java/com/salonflow/backend/controller/dtos/response/CustomerResponseDTO.java
@@ -1,22 +1,27 @@
 package com.salonflow.backend.controller.dtos.response;
 
+import com.salonflow.backend.controller.dtos.customer.CustomerCreateDTO;
 import com.salonflow.backend.domain.model.Customer;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record CustomerResponseDTO(UUID id,
+public record CustomerResponseDTO(
                                   String name,
-                                  String phone,
-                                  LocalDateTime created_at
+                                  String phone
                                   ) {
 
     public static CustomerResponseDTO toDTO(Customer customer){
         return new CustomerResponseDTO(
-                customer.getId(),
                 customer.getName(),
-                customer.getPhone(),
-                customer.getCreated_at()
+                customer.getPhone()
+        );
+    }
+
+    public static CustomerResponseDTO fromCreateToResponse(CustomerCreateDTO createDTO){
+        return new CustomerResponseDTO(
+                createDTO.name(),
+                createDTO.phone()
         );
     }
 }
