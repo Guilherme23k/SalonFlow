84931a97b433d69423f3ba00664552e384793467
Guilherme <guilhermeggr23@gmail.com>
2026-03-17 18:54:13 -0300

    refactor: add contract method for create professional (#7)

--- backend/src/main/java/com/salonflow/backend/service/ProfessionalService.java
+++ backend/src/main/java/com/salonflow/backend/service/ProfessionalService.java
@@ -1,9 +1,14 @@
 package com.salonflow.backend.service;
 
+import com.salonflow.backend.controller.dtos.professional.ProfessionalCreateDTO;
+import com.salonflow.backend.controller.dtos.response.ProfessionalResponseDTO;
 import com.salonflow.backend.domain.model.Professional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface ProfessionalService  {
+
+    ProfessionalResponseDTO create(ProfessionalCreateDTO dto);
+
 }
