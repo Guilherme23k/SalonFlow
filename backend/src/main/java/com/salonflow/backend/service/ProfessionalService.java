ef5bd7b0117ca74baae23ed0b9d10018eb3f3220
Guilherme <guilhermeggr23@gmail.com>
2026-03-30 21:58:30 -0300

    feat: add edit contract on ProfessionalService (#7)

--- backend/src/main/java/com/salonflow/backend/service/ProfessionalService.java
+++ backend/src/main/java/com/salonflow/backend/service/ProfessionalService.java
@@ -1,6 +1,7 @@
 package com.salonflow.backend.service;
 
 import com.salonflow.backend.controller.dtos.professional.ProfessionalCreateDTO;
+import com.salonflow.backend.controller.dtos.professional.ProfessionalDTO;
 import com.salonflow.backend.controller.dtos.response.ProfessionalResponseDTO;
 import com.salonflow.backend.domain.model.Professional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,7 @@ import java.util.UUID;
 public interface ProfessionalService  {
 
     ProfessionalResponseDTO create(ProfessionalCreateDTO dto);
+
+    ProfessionalResponseDTO edit(ProfessionalDTO dto);
 
 }
