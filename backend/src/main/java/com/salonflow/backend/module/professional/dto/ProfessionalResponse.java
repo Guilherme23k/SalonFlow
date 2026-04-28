8c3f4531b7bc25d3345884e0b552b7a1dabeb224
Guilherme <guilhermeggr23@gmail.com>
2026-04-28 18:26:38 -0300

    feat: added ProfessionalResponse

---
+++ backend/src/main/java/com/salonflow/backend/module/professional/dto/ProfessionalResponse.java
@@ -0,0 +1,26 @@
+package com.salonflow.backend.module.professional.dto;
+
+import com.salonflow.backend.module.professional.Professional;
+
+import java.util.UUID;
+
+public record ProfessionalResponse(
+
+        UUID id,
+        String name,
+        String phone,
+        Double commissionPercentage,
+        Boolean active
+) {
+
+    public ProfessionalResponse from (Professional professional){
+        return new ProfessionalResponse(
+                professional.getId(),
+                professional.getName(),
+                professional.getPhone(),
+                professional.getCommissionPercentage(),
+                professional.getActive()
+        );
+    }
+
+}
