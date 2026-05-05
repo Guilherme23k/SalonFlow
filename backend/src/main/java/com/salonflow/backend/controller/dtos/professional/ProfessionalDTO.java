430c36c01098b3fc10e703658f7a837746015bad
Guilherme <guilhermeggr23@gmail.com>
2026-05-04 22:31:49 -0300

    delete: removed not usable code

--- backend/src/main/java/com/salonflow/backend/controller/dtos/professional/ProfessionalDTO.java
+++
@@ -1,7 +0,0 @@
-package com.salonflow.backend.controller.dtos.professional;
-
-import com.salonflow.backend.validations.interfaces.BRPhoneNumber;
-import jakarta.validation.constraints.NotBlank;
-
-public record ProfessionalDTO(@NotBlank String name, @BRPhoneNumber String phone) {
-}
