273bbca4d6af14e2c3edc215fb71cc60fb72accc
Guilherme <guilhermeggr23@gmail.com>
2026-05-08 00:59:34 -0300

    feat: added SpringClientModule on app module

--- bff/src/app.module.ts
+++ bff/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { SpringClientModule } from './infra/http/spring-client.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    SpringClientModule,
   ],
   controllers: [AppController],
   providers: [AppService],
