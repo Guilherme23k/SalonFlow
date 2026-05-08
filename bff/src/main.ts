aafc9dc31eeca4265b9f87becd552bc57f491869
Guilherme <guilhermeggr23@gmail.com>
2026-05-08 01:00:08 -0300

    feat: added ports, filters and pipes on main (#14)

--- bff/src/main.ts
+++ bff/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`BFF rodando na porta ${port}`);
 }
 bootstrap();
