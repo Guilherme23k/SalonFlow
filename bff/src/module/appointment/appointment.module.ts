3969f232b843eea1568ecd5f9a1843054b400d56
Guilherme <guilhermeggr23@gmail.com>
2026-05-26 18:45:13 -0300

    feat: added springClientModule on appointment module

--- bff/src/module/appointment/appointment.module.ts
+++ bff/src/module/appointment/appointment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppointmentController } from './appointment.controller';
 import { AppointmentService } from './appointment.service';
+import { SpringClientModule } from '../../infra/http/spring-client.module';
 
 @Module({
+  imports: [SpringClientModule],
   controllers: [AppointmentController],
   providers: [AppointmentService],
 })
