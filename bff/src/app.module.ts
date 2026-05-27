6bd46eb92e48a33b17237271a5ac0cc90a770e5e
Guilherme <guilhermeggr23@gmail.com>
2026-05-27 17:07:42 -0300

    feat: added service, controller and tenant module on app module

--- bff/src/app.module.ts
+++ bff/src/app.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { SpringClientModule } from './infra/http/spring-client.module';
 import { AvailabilityModule } from './module/availability/availability.module';
 import { AppointmentModule } from './module/appointment/appointment.module';
+import { TenantModuleTsModule } from './module/tenant/tenant.module';
+import { TenantServiceTsService } from './module/tenant/tenant.service';
+import { TenantControllerTsController } from './module/tenant/tenant.controller';
 
 @Module({
   imports: [
@@ -12,6 +15,9 @@ import { AppointmentModule } from './module/appointment/appointment.module';
     SpringClientModule,
     AvailabilityModule,
     AppointmentModule,
+    TenantModuleTsModule,
   ],
+  providers: [TenantServiceTsService],
+  controllers: [TenantControllerTsController],
 })
 export class AppModule {}
