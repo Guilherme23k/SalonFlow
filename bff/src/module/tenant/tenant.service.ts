7eb374ccbe3a8893d5a9c88b3bc72cfec7e29768
Guilherme <guilhermeggr23@gmail.com>
2026-05-27 17:15:24 -0300

    feat: added findBySlug method on tenantService

--- bff/src/module/tenant/tenant.service.ts
+++ bff/src/module/tenant/tenant.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { SpringClientService } from '../../infra/http/spring-client.service';
+
+export interface TenantPublicResponse {
+  id: string;
+  name: string;
+  slug: string;
+  openingTime: string;
+  closingTime: string;
+}
 
 @Injectable()
-export class TenantService {}
+export class TenantService {
+  constructor(private readonly springClient: SpringClientService) {}
+
+  async findBySlug(slug: string): Promise<TenantPublicResponse> {
+    return this.springClient.get<TenantPublicResponse>(
+      `/api/v1/tenants/slug/${slug}`,
+      'public',
+    );
+  }
+}
