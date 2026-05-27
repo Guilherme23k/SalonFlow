0543d047c4d248de5f3cd24241a2b60dcf7a9eec
Guilherme <guilhermeggr23@gmail.com>
2026-05-26 23:00:29 -0300

    feat: added swagger descriptions on nestJS

--- bff/src/module/appointment/appointment.controller.ts
+++ bff/src/module/appointment/appointment.controller.ts
@@ -10,13 +10,21 @@ import {
 import { TenantInterceptor } from '../../common/interceptors/tenant.interceptor';
 import { AppointmentService } from './appointment.service';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
+import { ApiHeader, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Appointments')
+@ApiHeader({
+  name: 'x-tenant-id',
+  required: true,
+  description: 'UUID do tenant',
+})
 @Controller('api/v1/appointments')
 @UseInterceptors(TenantInterceptor)
 export class AppointmentController {
   constructor(private readonly appointmentService: AppointmentService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create new appointment' })
   async create(
     @Headers('x-tenant-id') tenantId: string,
     @Body() dto: CreateAppointmentDto,
@@ -25,6 +33,7 @@ export class AppointmentController {
   }
 
   @Patch(':id/cancel')
+  @ApiOperation({ summary: 'Cancel a appointment already existed' })
   async cancel(
     @Headers('x-tenant-id') tenantId: string,
     @Param('id') id: string,
--- bff/src/module/availability/availability.controller.ts
+++ bff/src/module/availability/availability.controller.ts
@@ -8,13 +8,38 @@ import {
 import { TenantInterceptor } from '../../common/interceptors/tenant.interceptor';
 import { AvailabilityService } from './availability.service';
 import { GetAvailabilityDTO } from './dto/get-availability.dto';
+import { ApiHeader, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Availability')
+@ApiHeader({
+  name: 'x-tenant-id',
+  required: true,
+  description: 'UUID do tenant',
+})
 @Controller('api/v1/availability')
 @UseInterceptors(TenantInterceptor)
 export class AvailabilityController {
   constructor(private readonly service: AvailabilityService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Lista slots disponíveis de um profissional em uma data',
+  })
+  @ApiQuery({
+    name: 'professionalId',
+    required: true,
+    description: 'UUID do profissional',
+  })
+  @ApiQuery({
+    name: 'serviceId',
+    required: true,
+    description: 'UUID do serviço',
+  })
+  @ApiQuery({
+    name: 'date',
+    required: true,
+    description: 'Data no formato YYYY-MM-DD',
+  })
   async getAvailability(
     @Headers('x-tenant-id') tenantId: string,
     @Query() dto: GetAvailabilityDTO,
