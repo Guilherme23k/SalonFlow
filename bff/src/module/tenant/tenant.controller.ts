0ad177a498db58d9b502186c0ca1f8003eb08bdc
Guilherme <guilhermeggr23@gmail.com>
2026-05-27 17:26:27 -0300

    feat: added findBySlug on tenantController

--- bff/src/module/tenant/tenant.controller.ts
+++ bff/src/module/tenant/tenant.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { TenantService } from './tenant.service';
 
-@Controller('tenant.controller.ts')
-export class TenantController {}
+@ApiTags('Tenant')
+@Controller('api/v1/tenants')
+export class TenantController {
+  constructor(private readonly tenantService: TenantService) {}
+
+  @Get('slug/:slug')
+  @ApiOperation({
+    summary: 'Search tenant by slug - used by frontend to solve the tenant url',
+  })
+  @ApiParam({
+    name: 'slug',
+    description: 'Slug of example salon: salao-do-guilherme',
+  })
+  async findBySlug(@Param('slug') slug: string) {
+    return this.tenantService.findBySlug(slug);
+  }
+}
