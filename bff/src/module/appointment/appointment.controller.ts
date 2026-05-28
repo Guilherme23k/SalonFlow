f4065faf3b21266c211cebc465a1c2a282f75595
Guilherme <guilhermeggr23@gmail.com>
2026-05-28 18:15:19 -0300

    remove: remove confict apiHeaders from appointment and availability controller (#19)

--- bff/src/module/appointment/appointment.controller.ts
+++ bff/src/module/appointment/appointment.controller.ts
@@ -10,14 +10,9 @@ import {
 import { TenantInterceptor } from '../../common/interceptors/tenant.interceptor';
 import { AppointmentService } from './appointment.service';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
-import { ApiHeader, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Appointments')
-@ApiHeader({
-  name: 'x-tenant-id',
-  required: true,
-  description: 'UUID do tenant',
-})
 @Controller('api/v1/appointments')
 @UseInterceptors(TenantInterceptor)
 export class AppointmentController {
