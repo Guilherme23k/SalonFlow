35f3fdd2f6d1b39ded4a99b7004b5e0d9abeee56
Guilherme <guilhermeggr23@gmail.com>
2026-05-28 18:39:45 -0300

    refactor: added routing on app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,20 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import BookingPage from "./pages/BookingPage"
 
-  return <div />
+export default function App(){
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/:slug" element={<BookingPage />} />
+      <Route
+      path="*"
+      element={
+        <div className="min-h-screen bg-zinc-950 flex items-center justify-center">
+          <p className="text-zinc-400 text-lg">Salão não encontrado</p>
+        </div>
+      }
+      />
+    </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App
