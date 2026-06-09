a86c60f7ec0db95176ac2737ec2c03b4edea2a9c
Guilherme <guilhermeggr23@gmail.com>
2026-06-09 19:13:04 -0300

    feat: added JwtService with T-004 methods (#5)

---
+++ backend/src/main/java/com/salonflow/backend/infra/security/jwt/JwtService.java
@@ -0,0 +1,78 @@
+package com.salonflow.backend.infra.security.jwt;
+
+import com.salonflow.backend.module.owner.Owner;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${application.security.jwt.secret-key:key}")
+    private String key;
+
+    @Value("${application.security.jwt.expiration:86400000}")
+    private Long jwtExpiration;
+
+    private Key getSignInKey(){
+        return Keys.hmacShaKeyFor(key.getBytes());
+    }
+
+    public String generateToken(Owner owner){
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("tenantId", owner.getTenantId());
+        extraClaims.put("userId", owner.getId());
+
+        return Jwts.builder()
+                .claims(extraClaims)
+                .subject(owner.getEmail())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractTenantId(String token) {
+        return extractClaim(token, claims -> claims.get("tenantId", String.class));
+    }
+
+    public boolean isTokenValid(String token){
+        try {
+            return !isTokenValid(token);
+        }
+        catch (Exception e){
+            return false;
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
