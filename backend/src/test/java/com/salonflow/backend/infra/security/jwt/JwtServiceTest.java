395a9415c95c1bebcee80353c501e68fbfb73203
Guilherme <guilhermeggr23@gmail.com>
2026-06-10 20:27:38 -0300

    refactor: added unit test must return false when token is invalid (#5)

--- backend/src/test/java/com/salonflow/backend/infra/security/jwt/JwtServiceTest.java
+++ backend/src/test/java/com/salonflow/backend/infra/security/jwt/JwtServiceTest.java
@@ -9,7 +9,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.sql.Ref;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,13 +21,12 @@ class JwtServiceTest {
     @Mock
     private Owner owner;
 
-    private final String testSecretKey = "46070d4bf934fb0d4b06d9e2c46e346944e322444900a435d7d9a95e6d7435f5";
-
     @BeforeEach
     void setup(){
         jwtService = new JwtService();
 
-        ReflectionTestUtils.setField(jwtService, "secretKey", testSecretKey);
+        String testSecretKey = "46070d4bf934fb0d4b06d9e2c46e346944e322444900a435d7d9a95e6d7435f5";
+        ReflectionTestUtils.setField(jwtService, "key", testSecretKey);
         ReflectionTestUtils.setField(jwtService, "jwtExpiration", 60000L);
     }
 
@@ -49,6 +47,28 @@ class JwtServiceTest {
 
         assertEquals(tenantId, jwtService.extractTenantId(token));
 
+    }
+
+    @Test
+    @DisplayName("Negative path: token with tenant changed")
+    void mustReturnFalseWhenTokenIsInvalid(){
+        UUID userId = UUID.randomUUID();
+        String tenantId = UUID.randomUUID().toString();
+
+        when(owner.getTenantId()).thenReturn(UUID.fromString(tenantId));
+        when(owner.getId()).thenReturn(userId);
+        when(owner.getEmail()).thenReturn("owner@email.com");
+
+        String validToken = jwtService.generateToken(owner);
+
+        String tokenInvalid = "eyJhbGciOiJIUzI1NiJ9.eyJ0ZW5hbnRJZCI6InRlbmFudC1hYmMtMTIzIiwidXNlcklkIjo0Miwic3ViIjoib3duZXJAZW1haWwuY29tIiwiaWF0IjoxNzgxMTEwNDAwLCJleHAiOjE3ODExOTY4MDB9.S3Y4bVJtc2VjcmV0S2V5Rm9ySFMyNTZTaWduaW5nMTIzNDU2Nzg";
+
+
+        boolean invalidToken = jwtService.isTokenValid(tokenInvalid);
+        boolean validTokenProof = jwtService.isTokenValid(validToken);
+
+        assertFalse(invalidToken);
+        assertTrue(validTokenProof);
     }
 
 
