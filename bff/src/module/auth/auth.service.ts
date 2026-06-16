e2a30347192b8fd2ffaa860643ba1746c05b260f
Guilherme <guilhermeggr23@gmail.com>
2026-06-16 00:46:27 -0300

    feat: added authService login and register methods (#27)

--- bff/src/module/auth/auth.service.ts
+++ bff/src/module/auth/auth.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { SpringClientService } from '../../infra/http/spring-client.service';
+import { RegisterRequest } from './dto/register.dto';
+import { LoginRequest } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly spring: SpringClientService) {}
+
+  async register(registerData: RegisterRequest): Promise<void> {
+    await this.spring.postAuth('/api/v1/auth', registerData);
+  }
+
+  async login(loginData: LoginRequest): Promise<string> {
+    const data = await this.spring.postAuth('/api/v1/auth', loginData);
+
+    if (!data) {
+      throw new UnauthorizedException('Failed to get auth token');
+    }
+
+    return data;
+  }
+}
